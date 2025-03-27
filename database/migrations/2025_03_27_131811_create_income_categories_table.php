e9338f2fd3761191533e078358e223cf28a80ec3
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2025-03-27 15:19:11 +0000

    tie user to categories

--- database/migrations/2025_03_27_131811_create_income_categories_table.php
+++ database/migrations/2025_03_27_131811_create_income_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('income_categories', function (Blueprint $table) {
             $table->id();
             $table->string('label')->nullable(false)->unique();
+            $table->integer('user_id')->references('id')->on('users')->nullable(false);
             $table->timestamps();
         });
     }
